14e5212f46de7d27332735281d60dc819c69c342
twallema <tallema2@jh.edu>
2026-01-04 21:36:15 +0100

    dirichlet-multinomial backfill extension

--- data/conversion/cases/backfill_preliminary-NHSN-HRD-data.py
+++ data/conversion/cases/backfill_preliminary-NHSN-HRD-data.py
@@ -20,6 +20,7 @@ from scipy.stats import beta as beta_dist
 # Define all paths reletive to this file
 abs_dir = os.path.dirname(__file__)
 
+
 ########################################
 ## Retrieve preliminary NHSN HRD data ##
 ########################################
@@ -59,36 +60,121 @@ for i in range(len(dfs) - 3 + 1):
         abs_backfill[f'influenza_admissions_{k+1}'] = data[k+1]['influenza_admissions_0']
     abs_backfill_collect.append(abs_backfill)
 
-###############################################################################
-## Fit a beta-binomial model to estimate average missigness of reported data ##
-###############################################################################
 
-# I've implemented a binomial thinning model to try and correct the backfill in the most recent (week X)
-# and second most recent (week X-1) preliminary NHSN HRD datapoints, when it is released in week X.
-# Backfill becomes practically negligible by week X-2. The model is a binomial thinning model with a Beta prior on the probability of success `p`,
-# ```
-# y_{X, X} ~ Binomial(y_{X, X+2}, p)
-# p ~ Beta(alpha, beta) with alpha=20, beta=1 so that E[p] \approx 0.95
-# ```
-# where `y_{X,X}` is the data of week X, released in week X, which is a fraction `p` of the "actual data" `y_{X, X+2}` which we only find after 2 weeks of backfilling.
-# After finding `p` by calibrating it to multiple observations, we can then attempt to estimate what the data will be in two weeks using,
-# ```
-# y*_{X, X} = y_{X, X} / p.
-# ```
-# The cool thing is this model's posterior probability mean has an analytical solution that is intuitive,
-# ```
-# Mean(p) = (alpha + sum_i[y_{X, X, i}]) / (beta + (sum_i[y_{X, X+2, i}] - sum_i[y_{X, X, i}]))
-# ```
-# Here `i` is used to index the datapoints (datapoints released at least two weeks prior so we have info on how big the backfilling was).
-# You just sum the influenza admissions upon release of the data up `sum_i[y_{X, X, i}]`, and you do the same to these data two weeks down the line `sum_i[y_{X, X+2, i}]`. For states with a lot of influenza admissions `sum_i[y_{X, X, i}] >> alpha, beta` this estimator is just the relative backfill, while states with more minor counts get pulled towards 95% completeness which buffers against noise.
-# I've implemented a similar procedure to correct `y_{X-1, X}` (previous week data, released this week) as a fraction of `y_{X-1, X+1}`.
-# Altough most `p` values here seem to fall in the 0.98-1.00 range indicating backfill is already negligible by week 2.
+#################################################################
+# Dirichlet–Multinomial Backfill Model (Mathematical Description)
+#################################################################
+#
+# Goal
+# ----
+# Estimate reporting completeness of influenza hospital admissions in the
+# most recent two preliminary NHSN HRD releases by modeling reporting delays.
+#
+# Data structure
+# --------------
+# For a given epidemiological week X, let:
+#
+#   y_{X,X}     = number of admissions in week X reported in week X (on release)
+#   y_{X,X+1}   = number of admissions in week X reported in week X+1 (after 1 week of backfill)
+#   y_{X,X+2}   = number of admissions in week X reported in week X+2 (after 2 weeks of backfill)
+#
+# We assume that by week X+2 the data are effectively final. We define reporting increments:
+#
+#   z_0 = y_{X,X}
+#   z_1 = y_{X,X+1} - y_{X,X}
+#   z_2 = y_{X,X+2} - y_{X,X+1}
+#   z_0 + z_1 + z_2 = y_{X,X+2}
+#
+# Model
+# -----
+# Conditional on the reporting-delay proportions \vec{pi} = (pi_0, pi_1, pi_2),
+# the incremental completion of the data follow a multinomial allocation of the final count,
+#
+#   (z_0, z_1, z_2) | π
+#       ~ Multinomial(
+#             n = y_{X,X+2},
+#             p = (pi_0, pi_1, pi_2)
+#         )
+#
+# where:
+#   pi_0 = fraction of cases reported immediately (week 0),
+#   pi_1 = fraction reported with a 1-week delay,
+#   pi_2 = fraction reported with a 2-week delay.
+#
+# Prior
+# -----
+# We place a Dirichlet prior on π:
+#
+#   (π_0, π_1, π_2) ~ Dirichlet(α_0, α_1, α_2)
+#
+# with parameters chosen to encode prior beliefs:
+#
+#   E[π_0]           ≈ 0.90   (≈90% completeness at release)
+#   E[π_0 + π_1]     ≈ 0.98   (≈98% completeness after 1 week)
+#
+# Achieved by setting:
+#
+#   α_k = kappa * m_k
+#
+# where:
+#   m = (0.95, 0.04, 0.01) is the prior mean vector,
+#   kappa > 0 controls prior strength (effective sample size).
+#
+# Posterior
+# ---------
+# By conjugacy, given aggregated increments z = (z_0, z_1, z_2),
+#
+#   (π_0, π_1, π_2) | z
+#       ~ Dirichlet(
+#             α_0 + z_0,
+#             α_1 + z_1,
+#             α_2 + z_2
+#         )
+#
+# Posterior means
+# ---------------
+# The posterior mean of each component is:
+#
+#   E[π_k | z]
+#       = (α_k + z_k) / (α_0 + α_1 + α_2 + z_0 + z_1 + z_2),
+#       for k = 0, 1, 2.
+#
+# Reporting completeness factors
+# ------------------------------
+# The quantities used for backfilling are cumulative completeness fractions:
+#
+#   p_02 = π_0
+#       = fraction of final cases observed at initial release,
+#
+#   p_12 = π_0 + π_1
+#       = fraction of final cases observed after 1 week.
+#
+# Their posterior means are therefore:
+#
+#   E[p_02 | z] = E[π_0 | z]
+#
+#   E[p_12 | z] = E[π_0 + π_1 | z]
+#               = E[π_0 | z] + E[π_1 | z]
+#
+# Backfill correction
+# -------------------
+# Let y_{X,X} and y_{X-1,X} denote the most recent and second-most-recent
+# preliminary observations available at release week X.
+#
+# These are backfilled to approximate the final (week X+2) counts by:
+#
+#   y*_{X,X}     = y_{X,X}     / E[p_02 | z]
+#   y*_{X-1,X}   = y_{X-1,X}   / E[p_12 | z]
+#
+# The model guarantees:
+#   0 ≤ p_02 ≤ p_12 ≤ 1,
+####################################################################
 
-# Beta distribution priors
-alpha_02 = 20
-beta_02 = 1    # E[p_i] = 0.95 (5% missing on release)
-alpha_12 = 50
-beta_12 = 1    # E[p_i] = 0.95 (5% missing on release)
+# Dirichlet priors
+kappa = 50  # regularization strength 
+alpha0 = 0.9 * kappa    # immediate
+alpha1 = 0.08 * kappa   # week +1
+alpha2 = 0.02 * kappa   # week +2
 
 # Aggregate dounts per state
 sum_df = pd.concat(abs_backfill_collect[-N:], ignore_index=True)
@@ -104,37 +190,28 @@ posterior = (
     )
 )
 
-# Compute posterior moments of p (0 --> 2)
-alpha_post = alpha_02 + posterior["y0_sum"]
-beta_post = beta_02 + (posterior["y2_sum"] - posterior["y0_sum"])
-posterior["p_02_mean"] = alpha_post / (alpha_post + beta_post)
-posterior["p_02_median"] = beta_dist.ppf(
-    0.5, alpha_post, beta_post
-)
-posterior["p_02_low_95"] = beta_dist.ppf(
-    0.025, alpha_post, beta_post
-)
-posterior["p_02_high_95"] = beta_dist.ppf(
-    0.975, alpha_post, beta_post
-)
+# Convert cumulative counts into increments
+posterior["z0"] = posterior["y0_sum"]
+posterior["z1"] = posterior["y1_sum"] - posterior["y0_sum"]
+posterior["z2"] = posterior["y2_sum"] - posterior["y1_sum"]
 
-# Compute posterior moments of p (1 --> 2)
-alpha_post = alpha_12 + posterior["y1_sum"]
-beta_post = beta_12 + (posterior["y2_sum"] - posterior["y1_sum"])
-posterior["p_12_mean"] = alpha_post / (alpha_post + beta_post)
-posterior["p_12_median"] = beta_dist.ppf(
-    0.5, alpha_post, beta_post
-)
-posterior["p_12_low_95"] = beta_dist.ppf(
-    0.025, alpha_post, beta_post
-)
-posterior["p_12_high_95"] = beta_dist.ppf(
-    0.975, alpha_post, beta_post
-)
+# Total final counts
+posterior["z_sum"] = posterior["z0"] + posterior["z1"] + posterior["z2"] # equal to y2_sum
+
+# Posterior means of Dirichlet components
+denom = alpha0 + alpha1 + alpha2 + posterior["z_sum"]
+posterior["pi0_mean"] = np.round((alpha0 + posterior["z0"]) / denom, 2)
+posterior["pi1_mean"] = np.round((alpha1 + posterior["z1"]) / denom, 2)
+posterior["pi2_mean"] = np.round((alpha2 + posterior["z2"]) / denom, 2)
+
+# Backfill completeness factors (drop-in replacements)
+posterior["p_02_mean"] = np.round(posterior["pi0_mean"], 2)
+posterior["p_12_mean"] = np.round(posterior["pi0_mean"] + posterior["pi1_mean"], 2)
 
 # Cap all cases where p > 1 to p = 1 (assumption that all retraction of cases represents an error)
 posterior["p_02_mean"] = posterior["p_02_mean"].clip(upper=1)
 posterior["p_12_mean"] = posterior["p_12_mean"].clip(upper=1)
+
 
 ###############################################
 ## Backfill latest preliminary NHSN HRD data ##
@@ -155,8 +232,6 @@ latest_df.loc[latest_df['date'] == latest_minus1_date, 'influenza admissions'] *
 # remove the p_02_mean and p_12_mean columns
 latest_df = latest_df.drop(columns=['p_02_mean', 'p_12_mean'])
 
-# deliberately chose not to round values --> we use a poisson likelihood function to fit the model which is continuous
-
 # put fips_state back in and sort
 posterior = posterior.merge(abs_backfill_collect[-1][['fips_state', 'name_state']], on='name_state')
 posterior = posterior.sort_values(by='fips_state')
@@ -166,7 +241,7 @@ posterior['start_backfill_window'] = sum_df['date'].unique()[0]
 posterior['end_backfill_window'] = sum_df['date'].unique()[-1]
 
 # slice out relevant columns
-posterior = posterior[['fips_state', 'name_state', 'start_backfill_window', 'end_backfill_window', 'p_02_mean', 'p_12_mean', 'p_02_median', 'p_12_median', 'p_02_low_95', 'p_12_low_95', 'p_12_high_95', 'p_12_high_95']]
+posterior = posterior[['fips_state', 'name_state', 'start_backfill_window', 'end_backfill_window', 'p_02_mean', 'p_12_mean']]
 
 # Save beta-binomial estimates and the data
 parquet_filenames = [os.path.basename(f) for f in parquet_files]
