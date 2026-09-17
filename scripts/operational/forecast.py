167caa97e8cd5b8078e6de2399116ea917cbbf4a
twallema <twa27@cornell.edu>
2026-09-17 14:41:23 -0400

    set to 12 cores

--- scripts/operational/forecast.py
+++ scripts/operational/forecast.py
@@ -46,7 +46,7 @@ multiplier_mcmc = 3                                             # Total number o
 print_n = 2000                                                  # Print diagnostics every `print_n`` iterations
 discard = 1500                                                  # Discard first `discard` iterations as burn-in
 thin = 10                                                      # Thinning factor emcee chains
-processes = 10 # int(os.environ.get('NUM_CORES', mp.cpu_count()))    # Number of CPUs to use
+processes = 12 # int(os.environ.get('NUM_CORES', mp.cpu_count()))    # Number of CPUs to use
 n = 1000                                                        # Number of simulations performed in MCMC goodness-of-fit figure
 
 # figure out what states to loop over
